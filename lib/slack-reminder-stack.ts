b06e673de833d87a0682cf32b792fb6f03cdc9a9
hiroya.shimamoto <shshimamo@gmail.com>
2021-09-25 04:41:57 +0900

    Add lambda handlers

--- lib/slack-reminder-stack.ts
+++ lib/slack-reminder-stack.ts
@@ -1,9 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import * as apigateway from "@aws-cdk/aws-apigateway";
 
 export class SlackReminderStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const appLambda = new NodejsFunction(this, "appLambda", {
+      entry: "src/lambda/handlers/app.ts",
+      handler: "handler",
+      environment: {
+        SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN || "",
+        SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET || "",
+      },
+    });
+
+    new apigateway.LambdaRestApi(this, "slackApi", {
+      handler: appLambda,
+    });
   }
 }
