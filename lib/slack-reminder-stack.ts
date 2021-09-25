965717fa0a09339b3fa9c814544a2ddc17e87f05
hiroya.shimamoto <shshimamo@gmail.com>
2021-09-25 23:35:50 +0900

    Deployment success

--- lib/slack-reminder-stack.ts
+++ lib/slack-reminder-stack.ts
@@ -1,22 +1,37 @@
 import * as cdk from '@aws-cdk/core';
-import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigateway from "@aws-cdk/aws-apigateway";
+
+const NODE_LAMBDA_LAYER_DIR = `${process.cwd()}/bundle`;
 
 export class SlackReminderStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const appLambda = new NodejsFunction(this, "appLambda", {
-      entry: "src/lambda/handlers/app.ts",
-      handler: "handler",
+    const nodeModulesLayer = new lambda.LayerVersion(this, 'NodeModulesLayer',
+      {
+        code: lambda.AssetCode.fromAsset(NODE_LAMBDA_LAYER_DIR),
+        compatibleRuntimes: [lambda.Runtime.NODEJS_14_X]
+      }
+    );
+
+    const appLambda = new lambda.Function(this, "appLambda", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: "hello.handler",
+      layers: [nodeModulesLayer],
       environment: {
         SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN || "",
         SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET || "",
       },
     });
 
-    new apigateway.LambdaRestApi(this, "slackApi", {
+    const api = new apigateway.LambdaRestApi(this, "slackApi", {
       handler: appLambda,
+      proxy: false
     });
+    const slack = api.root.addResource('slack');
+    const events = slack.addResource('events');
+    events.addMethod('POST');
   }
 }
