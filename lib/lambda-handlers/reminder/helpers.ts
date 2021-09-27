ea950d0db8c79101056ffe461867a7b3240a268a
hiroya.shimamoto <shshimamo@gmail.com>
2021-09-27 23:12:24 +0900

    Refactor

--- lib/lambda-handlers/reminder/helpers.ts
+++ lib/lambda-handlers/reminder/helpers.ts
@@ -1,9 +1,14 @@
 import {
+  AppOptions,
+  AwsLambdaReceiver,
+  LogLevel,
   ReactionAddedEvent,
-  ReactionRemovedEvent,
   ReactionMessageItem,
+  ReactionRemovedEvent,
   SlackEvent
 } from '@slack/bolt';
+import { ConversationsHistoryResponse } from "@slack/web-api/dist/response";
+import { WebClient } from '@slack/web-api'
 
 export const isReactionAddedEvent = (event: SlackEvent):
   event is ReactionAddedEvent => (event as ReactionAddedEvent).type === 'reaction_added';
@@ -13,3 +18,29 @@ export const isReactionRemovedEvent = (event: SlackEvent):
 
 export const isMessageItem = (item: ReactionAddedEvent['item'] | ReactionRemovedEvent['item']):
   item is ReactionMessageItem => (item as ReactionMessageItem).type === 'message';
+
+export const getConversionsHistory = async (channel: string, ts: string, client: WebClient):
+  Promise<ConversationsHistoryResponse> => {
+  return await client.conversations.history({
+    channel: channel,
+    latest: ts,
+    inclusive: true, // Limit the results to only one
+    limit: 1
+  });
+}
+
+export const slackAppOptions = (awsLambdaReceiver: AwsLambdaReceiver):
+  AppOptions => {
+  if (process.env.IS_LOCAL === "true") {
+    return {
+      token: process.env.SLACK_BOT_TOKEN ?? "",
+      signingSecret: process.env.SLACK_SIGNING_SECRET,
+      logLevel: LogLevel.DEBUG
+    }
+  }
+  return {
+    token: process.env.SLACK_BOT_TOKEN ?? "",
+    receiver: awsLambdaReceiver,
+    processBeforeResponse: true
+  }
+}
--- lib/lambda-handlers/reminder/reminder.ts
+++ lib/lambda-handlers/reminder/reminder.ts
@@ -1,86 +1,29 @@
-import {
-  App,
-  AppOptions,
-  AwsLambdaReceiver,
-  LogLevel,
-} from '@slack/bolt';
+import { App, AwsLambdaReceiver } from '@slack/bolt';
 import { ConversationsHistoryResponse } from "@slack/web-api/dist/response";
 import { Message } from "@slack/web-api/dist/response/ConversationsHistoryResponse";
 import { DynamoDB } from 'aws-sdk';
-import {
-  isReactionAddedEvent,
-  isMessageItem,
-  isReactionRemovedEvent
-} from './helpers';
+import * as helpers from './helpers';
 
-// Initialize your custom receiver
 const awsLambdaReceiver = new AwsLambdaReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET ?? "",
 });
-
-// Initializes your app with your bot token and the AWS Lambda ready receiver
-const appArgs: AppOptions = { token: process.env.SLACK_BOT_TOKEN ?? "" };
-if (process.env.IS_LOCAL === "true") {
-  appArgs.signingSecret = process.env.SLACK_SIGNING_SECRET
-  appArgs.logLevel = LogLevel.DEBUG
-} else {
-  appArgs.receiver = awsLambdaReceiver
-  appArgs.processBeforeResponse = true
-}
-const app = new App(appArgs);
+const app = new App(helpers.slackAppOptions(awsLambdaReceiver));
 
 app.event('reaction_added', async ({ event, client }) => {
-  if (!isReactionAddedEvent(event)) return;
-  if (!isMessageItem(event.item)) return;
+  if (!helpers.isReactionAddedEvent(event)) return;
+  if (!helpers.isMessageItem(event.item)) return;
 
   const channel = event.item.channel
+  const ts = event.item.ts
   const dynamo = new DynamoDB.DocumentClient();
-  if (event.reaction.match(/.*remind_[0-9]+_[mh]$/)) {
-    const result: ConversationsHistoryResponse = await client.conversations.history({
-      channel: channel,
-      latest: event.item.ts,
-      inclusive: true, // Limit the results to only one
-      limit: 1
-    });
-    // There should only be one result (stored in the zeroth index)
-    const message: Message | undefined = result.messages?.[0];
-    if (!message || !message.text) return;
 
-    const mention_user_names = message.text.match(/<@[^\s]+>/g);
-    if (!mention_user_names) return;
-
-    // DynamoDB
-    const now = (new Date()).getTime();
-    for (const mention_user_name of mention_user_names) {
-      await dynamo
-        .put({
-          TableName: process.env.REMINDER_TABLE_NAME ?? "",
-          Item: {
-            "MentionedUser": mention_user_name.slice(2, -1),
-            "ChannelAndMessageTs": `${channel}_${event.item.ts}`,
-            "LastRemindedAt": now,
-            "MessageLink": `https://${process.env.SLACK_WORKSPACE}.slack.com/archives/${channel}/p${event.item.ts.replace(/\./g, "")}`,
-            "ReactionUser": event.user,
-            "IsFinished": "false",
-            "Reaction": event.reaction
-          }
-        })
-        .promise()
-    }
-  } else if (event.reaction.match(/remind_finish/)) {
+  if (event.reaction.match(/remind_finish/)) {
     // when finish reaction added
-    const result: ConversationsHistoryResponse = await client.conversations.history({
-      channel: channel,
-      latest: event.item.ts,
-      inclusive: true, // Limit the results to only one
-      limit: 1
-    });
-
     await dynamo.update({
       TableName: process.env.REMINDER_TABLE_NAME ?? "",
       Key: {
         "MentionedUser": event.user,
-        "ChannelAndMessageTs": `${channel}_${event.item.ts}`
+        "ChannelAndMessageTs": `${channel}_${ts}`
       },
       UpdateExpression: "set IsFinished = :if",
       ExpressionAttributeValues: {
@@ -88,37 +31,31 @@ app.event('reaction_added', async ({ event, client }) => {
       },
       ReturnValues: "UPDATED_NEW"
     }).promise();
+    return
   }
-});
-
-app.event('reaction_removed', async ({ event, client }) => {
-  if (!isReactionRemovedEvent(event)) return;
-  if (!isMessageItem(event.item)) return;
 
   if (event.reaction.match(/.*remind_[0-9]+_[mh]$/)) {
-    const channel = event.item.channel
-    const result: ConversationsHistoryResponse = await client.conversations.history({
-      channel: channel,
-      latest: event.item.ts,
-      inclusive: true, // Limit the results to only one
-      limit: 1
-    });
-    // There should only be one result (stored in the zeroth index)
-    const message: Message | undefined = result.messages?.[0];
-    if (!message || !message.text || !message.ts) return;
+    const conversation: ConversationsHistoryResponse = await helpers.getConversionsHistory(channel, ts, client);
+
+    const message: Message | undefined = conversation.messages?.[0];
+    if (!message?.text) return;
 
     const mention_user_names = message.text.match(/<@[^\s]+>/g);
     if (!mention_user_names) return;
 
-    // DynamoDB
-    const dynamo = new DynamoDB.DocumentClient();
+    const now = (new Date()).getTime();
     for (const mention_user_name of mention_user_names) {
       await dynamo
-        .delete({
+        .put({
           TableName: process.env.REMINDER_TABLE_NAME ?? "",
-          Key: {
+          Item: {
             "MentionedUser": mention_user_name.slice(2, -1),
-            "ChannelAndMessageTs": `${channel}_${message.ts}`
+            "ChannelAndMessageTs": `${channel}_${ts}`,
+            "LastRemindedAt": now,
+            "MessageLink": `https://${process.env.SLACK_WORKSPACE}.slack.com/archives/${channel}/p${ts.replace(/\./g, "")}`,
+            "ReactionUser": event.user,
+            "IsFinished": "false",
+            "Reaction": event.reaction
           }
         })
         .promise()
@@ -126,13 +63,35 @@ app.event('reaction_removed', async ({ event, client }) => {
   }
 });
 
-// Listens to incoming messages that contain "goodbye"
-app.message('goodbye', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say(`See ya later, <@${(message as any).user}> :wave:`);
+app.event('reaction_removed', async ({ event, client }) => {
+  if (!helpers.isReactionRemovedEvent(event)) return;
+  if (!helpers.isMessageItem(event.item)) return;
+
+  if (!event.reaction.match(/.*remind_[0-9]+_[mh]$/)) return;
+
+  const channel = event.item.channel
+  const ts = event.item.ts
+  const conversation: ConversationsHistoryResponse = await helpers.getConversionsHistory(channel, ts, client);
+  const message: Message | undefined = conversation.messages?.[0];
+  if (!message?.text) return;
+
+  const mention_user_names = message.text.match(/<@[^\s]+>/g);
+  if (!mention_user_names) return;
+
+  const dynamo = new DynamoDB.DocumentClient();
+  for (const mention_user_name of mention_user_names) {
+    await dynamo
+      .delete({
+        TableName: process.env.REMINDER_TABLE_NAME ?? "",
+        Key: {
+          "MentionedUser": mention_user_name.slice(2, -1),
+          "ChannelAndMessageTs": `${channel}_${ts}`
+        }
+      })
+      .promise()
+  }
 });
 
-// Handle the Lambda function event
 export const handler = async (event: any, context: any, callback: any) => {
   const handler = await awsLambdaReceiver.start();
   return handler(event, context, callback);
